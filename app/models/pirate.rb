25e5d547ce7c235e728acaea2a156b50383a0b62
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-04-23 21:28:44 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,10 +3,10 @@ class Pirate
   
   @@all = []
   
-  def initialize(name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
     @@all << self
   end
   
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,10 +3,10 @@ class Ship
   
   @@all = []
   
-  def initialize(name, type, booty)
-    @name = name
-    @type = type
-    @booty = booty
+  def initialize(args)
+    @name = args[:name}
+    @type = args[:type]
+    @booty = args[:booty]
     @@all << self
   end
   
